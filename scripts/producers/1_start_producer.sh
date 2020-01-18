2ad027535c0267153ff3187169ab5a6ad34699f7
Erick <eum602@gmail.com>
2020-01-18 18:00:56 -0500

    added properties like acks when creating producers

--- scripts/producers/1_start_producer.sh
+++ scripts/producers/1_start_producer.sh
@@ -1,3 +1,4 @@
 broker_url=127.0.0.1:9092
 topic_name=first_topic
-kafka-console-producer.sh --broker-list $broker_url --topic $topic_name
+kafka-console-producer.sh --broker-list $broker_url --topic $topic_name \
+--producer-property acks=all
