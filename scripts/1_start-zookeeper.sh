e6c3e9fbcc7f28fd69553553dfe1f61cf62097f1
Erick <eum602@gmail.com>
2020-01-18 16:53:23 -0500

    added scripts to start zookeeper and kafka

---
+++ scripts/1_start-zookeeper.sh
@@ -0,0 +1,1 @@
+zookeeper-server-start.sh ~/kafka/config/zookeeper.properties #starts zookeeper at port 2181
---
+++ scripts/2_start-kafka.sh
@@ -0,0 +1,3 @@
+kafka-server-start.sh ~/kafka/config/server.properties
+#data is stored in ~/kafka/data/kafka
+#kafkaServer id=0 ==> indicates all go well
