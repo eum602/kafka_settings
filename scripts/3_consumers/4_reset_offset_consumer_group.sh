95f7d8668a868a2d3ac2796625c9a2d4442128e3
Erick <eum602@gmail.com>
2020-01-19 11:44:50 -0500

    added examples about resetting offsets in topics => shift-by, to-earliest, to-latest, etc

---
+++ scripts/3_consumers/4_reset_offset_consumer_group.sh
@@ -0,0 +1,14 @@
+kafka_server_1=127.0.0.1:9092
+topic=first_topic
+group_name=my-group
+
+kafka-consumer-groups.sh --bootstrap-server $kafka_server_1 --group $group_name --reset-offsets --to-earliest --execute --topic $topic
+
+: <<'END'
+By using --reset-offset you have to specify the from where to reset the  offseto. When running the consumer will read the data from the modified current_offset in all partitions. There are many options for that: --to-earliest, to-latest, etc.
+--to-earliest ==> resets CURRENT-OFFSET to ZERO
+--shift-by 2 ; -2; -3, -6 and so on. THis allows to move forward or backwards the current_offset.
+--execute => to update the offset reset , you can check this by simply using script 3_describe_consumer_group.sh
+--all-topics or --topic
+END
+
