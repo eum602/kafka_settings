d09624de85223978b52b11d8f2aef72c32a638be
Erick <eum602@gmail.com>
2020-01-18 19:36:24 -0500

    explained how to load not only real time incomming messages but also all messages within the topic

--- scripts/consumers/1_start_consumer.sh
+++ scripts/consumers/1_start_consumer.sh
@@ -1,3 +1,4 @@
 kafka_server_1=127.0.0.1:9092
 topic=first_topic
-kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic 
+kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic --from-beginning 
+# By adding --from-beginning option consumer not only shows current incomming messages but also all past messages. Note that messages are not ordered, to see total orderering try to see some particular partition where you will see total ordering. 
