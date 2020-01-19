e6a608231dd4ede2fcae051b534d3c399b47a136
Erick <eum602@gmail.com>
2020-01-19 09:48:26 -0500

    explained how consumer reads data from where it let off the last time- achieved by way of CONSUMER OFFSETS

--- scripts/3_consumers/1_start_consumer.sh
+++ scripts/3_consumers/1_start_consumer.sh
@@ -1,6 +1,7 @@
 kafka_server_1=127.0.0.1:9092
 topic=first_topic
-kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic --group my-group
+group_name=my-group
+kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic --group $group_name --from-beginning
 #adding --group with some name creates a set with all consumers who run the kafka consumer with the same group id. To test this functionality run this command in various terminals and send messages from a producer you will see the following:
 	#if only one consumer is started then all messages from all partitions will go there.
 	#if you increase to more than one xonsumer then all messages gets distributed to the consumers.
@@ -10,3 +11,5 @@ kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic --gr
 #messages but also all past messages. Note that messages are not ordered, to see total orderering try to see
 #some particular partition where you will see total ordering.
 #THE CONSUMER GROUP REBALANCE AND SHARE THE LOAD BETWEEN ALL THE CONSUMERS.
+
+#According to consumer offsets, when some consumer reads data from kafka then a commit offset occurs. As a result, if we read all the data (--from-beginning) in some consumer that is into some group, then all other consumers into that group won't read again that message because the offset has moved forward.
