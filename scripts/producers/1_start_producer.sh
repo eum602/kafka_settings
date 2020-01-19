504085f186f946f5c7d90abd64d79f480d094464
Erick <eum602@gmail.com>
2020-01-18 19:00:01 -0500

    explained possibility to indicate a topic that does not exist when starting sending messages in a producer

--- scripts/producers/1_start_producer.sh
+++ scripts/producers/1_start_producer.sh
@@ -1,4 +1,10 @@
 broker_url=127.0.0.1:9092
-topic_name=first_topic
+topic_name=topic_from_producer #first_topic
 kafka-console-producer.sh --broker-list $broker_url --topic $topic_name \
 --producer-property acks=all
+
+#specifying a topic that is not created by way of kafka-topics can be created from here, but replicas and partition configuration will default to 1; this is not recomended, instead only refer to topics that were correctly created and configured by way of kafka-topics tool.
+#To modify the default properties  go to ~/kafka/config/server.properties
+#then modify this: num.partitions=1 ==> num.partitions=3 so now when a new topic is created, lets say by usin a producer then that topic will have by default three partitions. ==> after changing restart kafka
+#By creating a topic by using producers will throw a warn after that you wil be ableto send messages.
+#You can see the properties of a topic by listing the topics with kafka-topics ...--list
