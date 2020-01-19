5d19185a44cbea50c4241f5f20f6d578e0682032
Erick <eum602@gmail.com>
2020-01-19 09:53:17 -0500

    explained concept of consumer offsets after all consumers go down and then up

--- scripts/3_consumers/1_start_consumer.sh
+++ scripts/3_consumers/1_start_consumer.sh
@@ -1,7 +1,7 @@
 kafka_server_1=127.0.0.1:9092
 topic=first_topic
 group_name=my-group
-kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic --group $group_name --from-beginning
+kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic --group $group_name #--from-beginning
 #adding --group with some name creates a set with all consumers who run the kafka consumer with the same group id. To test this functionality run this command in various terminals and send messages from a producer you will see the following:
 	#if only one consumer is started then all messages from all partitions will go there.
 	#if you increase to more than one xonsumer then all messages gets distributed to the consumers.
@@ -13,3 +13,4 @@ kafka-console-consumer.sh --bootstrap-server $kafka_server_1 --topic $topic --gr
 #THE CONSUMER GROUP REBALANCE AND SHARE THE LOAD BETWEEN ALL THE CONSUMERS.
 
 #According to consumer offsets, when some consumer reads data from kafka then a commit offset occurs. As a result, if we read all the data (--from-beginning) in some consumer that is into some group, then all other consumers into that group won't read again that message because the offset has moved forward.
+#Another conclusion is that when all consumers stop then when they reconnects they will read the messages from the position of consumer offset, so if messages are stacked then all those will be read and consumer offset will be updated to a new position in order to not to read those messages again.
