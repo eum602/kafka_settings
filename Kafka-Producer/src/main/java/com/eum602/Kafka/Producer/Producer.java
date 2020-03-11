17d0436b6bf50f754e443faeb73a785a7f472d32
Erick <eum602@gmail.com>
2020-03-10 23:51:51 -0500

    added code to exemplify that when key is defined, then for a fixed number of partitions then each key is always mapped to a partition, eg. key_1 => partition0; key_2 => partition2 and so on

--- Kafka-Producer/src/main/java/com/eum602/Kafka/Producer/Producer.java
+++ Kafka-Producer/src/main/java/com/eum602/Kafka/Producer/Producer.java
@@ -6,9 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class Producer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Logger logger = LoggerFactory.getLogger(Producer.class);
         String bootstrapServers =  "127.0.0.1:9092";
         //Create producer properties
@@ -22,10 +23,15 @@ public class Producer {
         //Create the producer
         KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties); //We want the key and value to be a string
 
-        //Create a producer record
         for (int i=0; i<10;i++){
-            ProducerRecord<String,String> record =  new ProducerRecord<>("first_topic", "message #" + Integer.toString(i));
+            //Create a producer record
+            String topic = "first_topic";
+            String value = "message #" + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+            ProducerRecord<String,String> record =  new ProducerRecord<>(topic, key, value); // ==> the same keys goes to the same partition when a key us assigned
             //as we do not use any keys then all the messages will go to random partitions. What is more all messages will go to all partitions.
+
+            logger.info("Key:" + key);
 
             //send data
             producer.send(record, new Callback() {
@@ -43,8 +49,16 @@ public class Producer {
                         logger.error("Error while producing" + exception);
                     }
                 }
-            });
+            }).get(); //only for demo NOT for production ==>write this ".get" then press alt + enter ==> which will give you the opportunity to add an exception to this function
+            //what ".get()" does is to clock the .send() method in order to make this function synchronous.
         }
+        /*
+        * id_0 => partition 1
+        * id_1 => partition 0
+        * id_2 => partition 2
+        * id_3 => partition 0
+        * ... ==> this order will be deterministic because when using keys each key is always sent to the same partition.
+        * */
 
         //flush code
         producer.flush(); //wait for the data to be produced
