d23c72b97a53ab67ebd1ea89ce38c5dab63a16ef
Erick <eum602@gmail.com>
2020-04-05 00:13:30 -0500

    deleted restructured files

--- Kafka-Producer/src/main/java/com/eum602/Kafka/Twitter/Env.java
+++
@@ -1,8 +0,0 @@
-package com.eum602.Kafka.Twitter;
-
-public class Env {
-    protected static String CONSUMER_KEY =  "9Rrc2eupWoKTPdqf8rhWrAphl";
-    protected static String CONSUMER_SECRET = "aQMhGVSMcGEPHoUEqFpFVwZrd7Bmo4OnTa7voye6pNNLfyHnme";
-    protected static String TOKEN = "1799023009-hm9ua0aX2AtJZyQXVap4X5fvIsj9Z4GSpETvRMJ";
-    protected static String SECRET = "sBeLo2E0sUClGhrqPtaTDV5dwfxZgSewW1fMaSCJfbQH8";
-}
